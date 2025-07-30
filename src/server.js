c5e78e992f612612b8fda9366ae36671f9109b3b
abhigyan2003 <abhigyanborah3@gmail.com>
2025-07-30 20:09:26 +0530

    added cron

--- src/server.js
+++ src/server.js
@@ -4,11 +4,13 @@ import { initDB } from './config/db.js';
 import rateLimiter from './middleware/rateLimiter.js';
 import transactionsRoute from './routes/transactionsRoute.js';
 import { neon } from '@neondatabase/serverless';
-
+import job from './config/cron.js';
 
 dotenv.config();
 
 export const app = express();
+
+if(process.env.NODE_ENV === "production") job.start();
 
 // Middleware
 app.use(rateLimiter);
@@ -16,6 +18,10 @@ app.use(express.json());
 
 const PORT = process.env.PORT || 5001;
 
+
+app.get("/api/health", (req, res) => {
+  res.status(200).json({ status: "ok" });
+});
 // Initialize DB
 
 
