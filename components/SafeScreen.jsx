59afe789c88eeee145475bd3118a39f0434e98c5
abhigyan2003 <abhigyanborah3@gmail.com>
2025-07-30 21:29:01 +0530

    react native expo frontend

---
+++ components/SafeScreen.jsx
@@ -0,0 +1,17 @@
+import { View, Text } from 'react-native'
+import React from 'react'
+import { useSafeAreaInsets } from 'react-native-safe-area-context';
+import { COLORS } from '../constants/colors';
+
+const SafeScreen = ({children}) => {
+
+    const insets = useSafeAreaInsets();
+  return (
+    <View style={{paddingTop: insets.top, backgroundColor:COLORS.background, flex: 1}}>
+   
+      {children}
+    </View>
+  )
+}
+
+export default SafeScreen;
---
+++ constants/api.js
@@ -0,0 +1,4 @@
+export const API_URL = "https://wallet-rn-bs7n.onrender.com/api";
+
+// export const API_URL = "http://localhost:5001/api";
+
